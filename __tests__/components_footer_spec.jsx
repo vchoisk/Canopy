d884d617cce7c503c836203ff3e5b3a782277a6f
Sean Enright <seanenright86@gmail.com>
2016-12-15 19:50:31 -0800

    [add] component test - footer

--- __tests__/components_footer_spec.jsx
+++ __tests__/components_footer_spec.jsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Footer from '../public/components/footer';
 
 describe('<Footer />', () => {
-  it('should render something right?', () => {
+  it('should render 5 list items', () => {
     const wrapper = shallow(<Footer />);
     expect(wrapper.find('li').length).toBe(5);
   });
